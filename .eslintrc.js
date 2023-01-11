8828f8607b4acc0e972a964930e9cb9a5cf7e1f1
younsoo <tweatershin@gmail.com>
2023-01-11 11:07:55 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: [
+		"plugin:react/recommended",
+
+		"airbnb",
+		"react-app/jest",
+		"plugin:prettier/recommended",
+		"prettier",
+		// "ddfdfds",
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+	},
+	plugins: ["react", "prettier"],
+	rules: {
+		"react/jsx-filename-extension": [1, { extensions: ["js", "jsx"] }],
+		"no-param-reassign": 0,
+		"arrow-parens": 0,
+		"react/prop-types": 0,
+		"comma-dangle": 0,
+		"object-curly-newline": 0,
+		"no-unused-vars": "warn",
+		"react/jsx-props-no-spreading": [1, { custom: "ignore" }],
+		"react/react-in-jsx-scope": 0,
+		"import/prefer-default-export": 0,
+		"react-hooks/exhaustive-deps": 0,
+		"react/destructuring-assignment": 0,
+		"react/no-access-state-in-setstate": 0,
+		"jsx-a11y/no-static-element-interactions": 0,
+		"jsx-a11y/click-events-have-key-events": 0,
+	},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  "printWidth": 130,
+  "useTabs": true,
+  "semi": true,
+  "singleQuote": false,
+  "quoteProps": "as-needed",
+  "jsxSingleQuote": false,
+  "arrowParens": "avoid",
+  "requirePragma": false,
+  "insertPragma": false,
+  "proseWrap": "preserve",
+  "htmlWhitespaceSensitivity": "css",
+  "endOfLine": "auto",
+  "bracketSpacing": true,
+  "jsxBracketSameLine": true,
+  "trailingComma": "all",
+  "parser": "babel",
+  "tabWidth": 2
+}
+
